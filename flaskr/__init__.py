6fdc3306b5533fb2e707cd1d9b2bf8950adb4bac
Ciprian Chende-Roman <cip_chende@yahoo.com>
2025-02-12 22:18:01 +0200

    __init__.py pentru versiunea 2 ... -v1.0.0. app in dir - minimum necesar

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,23 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
 
+    app = Flask(__name__)
+    print("__name__ = ", __name__)
     @app.route('/')
     def hello():
         return "SALUT"
     
     return app
+    '''
+    #app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True) 
+    # incarca fisierul de configurare din directorul 'instance', nu din root
+    # fara acest parametru, se va incerca incarcarea config.py din 'flaskr'
+    # cu acesta - din directorul 'instance' care este in afara 'flaskr'
+    print("app.instance_path:", app.instance_path)
+    print("app.root_path:    ", app.root_path)
+
+    app.config.from_pyfile('config.py', silent=False)
+    print(app.config)
+    '''
